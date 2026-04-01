1249957ee2b013643c0fb4e2ae00f73730f8ce0b
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-04-01 09:34:44 +0200

    feat(webshop): hash du mdp + sel

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,23 +1,15 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 module.exports = {
-  // ----------------------------------------------------------
-  // POST /api/auth/login
-  // ----------------------------------------------------------
   login: (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ error: "Email et mot de passe requis" });
-    }
-
-    //requête SQL avec parametre passés en tableau
-    const query = "SELECT * FROM users WHERE email = ? AND password = ?";
-
-    db.query(query, [email, password], (err, results) => {
-      if (err) {
-        return res.status(500).json({ error: err.message, query: query });
-      }
+    //on cherche l'utilisateur par son email
+    const query = "SELECT * FROM users WHERE email = ?";
+    db.query(query, [email], async (err, results) => {
+      if (err) return res.status(500).json({ error: err.message });
 
       if (results.length === 0) {
         return res
@@ -25,22 +17,32 @@ module.exports = {
           .json({ error: "Email ou mot de passe incorrect" });
       }
 
-      // Connexion réussie
-      res.json({ message: "Connexion réussie" });
+      const user = results[0];
+
+      //on compare le mdp saisi avec le hash en db
+      const match = await bcrypt.compare(password, user.password);
+
+      if (match) {
+        res.json({ message: "Connexion réussie" });
+      } else {
+        res.status(401).json({ error: "Email ou mot de passe incorrect" });
+      }
     });
   },
 
   // ----------------------------------------------------------
   // POST /api/auth/register
   // ----------------------------------------------------------
-  register: (req, res) => {
+  register: async (req, res) => {
     const { username, address, email, password } = req.body;
+
+    //on hash le mot de passe avant de le insert
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const query =
       "INSERT INTO users (username, address, email, password) VALUES (?, ?, ?, ?)";
-
-    db.query(query, [username, address, email, password], (err) => {
+    db.query(query, [username, address, email, hashedPassword], (err) => {
       if (err) return res.status(500).send(err.message);
-
       res.json({ message: "Utilisateur créé !" });
     });
   },
