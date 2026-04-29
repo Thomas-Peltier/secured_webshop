4e988d67cc01f6c3a06e3bafc2ca7e3001508b75
Thomas-Peltier <pv03xgo@eduvaud.ch>
2026-04-29 10:16:14 +0200

    feat(https): ajout du https avec mkcert

--- app/server.js
+++ app/server.js
@@ -1,18 +1,17 @@
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 
 require("dotenv").config({ path: path.resolve(__dirname, "..", ".env") });
 
 const express = require("express");
 
 const app = express();
-
 // Middleware pour parser le corps des requêtes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Fichiers statiques (CSS, images, uploads...)
 app.use(express.static(path.join(__dirname, "public")));
-
 // ---------------------------------------------------------------
 // Routes API (retournent du JSON)
 // ---------------------------------------------------------------
@@ -23,7 +22,6 @@ const adminRoute = require("./routes/Admin");
 app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/admin", adminRoute);
-
 // ---------------------------------------------------------------
 // Routes pages (retournent du HTML)
 // ---------------------------------------------------------------
@@ -46,8 +44,13 @@ app.get("/admin", (_req, res) =>
   res.sendFile(path.join(__dirname, "views", "admin.html")),
 );
 
-// Démarrage du serveur
 app.get("/test", (_req, res) => res.send("db admin: root, pwd : root"));
-app.listen(8080, () => {
-  console.log("Serveur démarré sur http://localhost:8080");
+
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, "localhost+1-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "localhost+1.pem")),
+};
+
+https.createServer(sslOptions, app).listen(8080, () => {
+  console.log("Serveur démarré sur https://localhost:8080");
 });
